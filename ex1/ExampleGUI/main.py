d75b714db5d256143f345e44e33aec6a47785e1b
Wenbin HU <whuak@connect.ust.hk>
2023-04-25 19:14:47 +0200

    finish restart and multi-step

--- ex1/ExampleGUI/main.py
+++ ex1/ExampleGUI/main.py
@@ -3,10 +3,12 @@ import argparse
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    poison_setting = parser.add_argument_group("poison settings")
+    cms_config = parser.add_argument_group("poison settings")
 
-    poison_setting.add_argument("--json_path", required=False, default="../scenarios/sc0.json",
+    cms_config.add_argument("--json_path", required=False, default="../scenarios/sc0.json",
                     help="json file path.") 
+    cms_config.add_argument("--iter", default=100, type=int, required=False,
+                    help="set the number of iteration steps.") 
     args = parser.parse_args()
     return args
 
