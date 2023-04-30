62ef7e162be002823cf7bba5da08fb4006d616a5
Daniel <daniel.bamberger@tum.de>
2023-04-30 03:32:43 +0200

    Combine Speed and diagonal movement

--- ex1/ExampleGUI/scenario_elements.py
+++ ex1/ExampleGUI/scenario_elements.py
@@ -17,7 +17,8 @@ class Pedestrian:
         self._desired_speed = desired_speed
 
         self.status = 'walking' # walking or finished
-        self._before_schedule = 0
+        self.desired_walked_distance = 0
+        self.walked_distance = 0
 
         self._track = []
         self.step_length = desired_speed * 1
@@ -55,7 +56,6 @@ class Pedestrian:
     
 
     def get_next_position(self, scenario):
-
         neighbors = self.get_neighbors(scenario)
         cost = copy.deepcopy(scenario.cost)
         scenario.individual_repulse_force(cost, self._position[0], self._position[1], sign=-1)
@@ -84,22 +84,21 @@ class Pedestrian:
         """
         next_pos = self.get_next_position(scenario)
         self.move_dist = ((self.position[0] - next_pos[0]) ** 2 + (self.position[1] - next_pos[1]) ** 2) ** (1/2)
-        if self.move_dist <= self.step_length * self.accumulate_steps:
-            while self.move_dist <= self.step_length * self.accumulate_steps:
-                self._position = next_pos
-                for tar in scenario.target_list:
-                    if (self._position[0], self._position[1]) == (tar[0], tar[1]):
-                        self.status = 'finished'     
-                        return
-                next_pos = self.get_next_position(scenario)
-                if (self.position[0] - next_pos[0]) ** 2  == (self.position[1] - next_pos[1]):
-                    break
-                self.move_dist += ((self.position[0] - next_pos[0]) ** 2 + (self.position[1] - next_pos[1]) ** 2 ) ** (1/2)
-            self.move_dist = 0 
-            self.accumulate_steps = 1
-        else:
-            self.accumulate_steps += 1
+        self.desired_walked_distance += self.desired_speed
 
+        while self.walked_distance + self.move_dist <= self.desired_walked_distance + 0.5:
+            self._position = next_pos
+            self.walked_distance += self.move_dist
+            for tar in scenario.target_list:
+                if (self._position[0], self._position[1]) == (tar[0], tar[1]):
+                    self.status = 'finished'
+                    return
+            next_pos = self.get_next_position(scenario)
+            self.move_dist = ((self.position[0] - next_pos[0]) ** 2 + (self.position[1] - next_pos[1]) ** 2 ) ** (1/2)
+
+
+        self.move_dist = 0
+        self.accumulate_steps = 1
 
 
 
